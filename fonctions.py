978bd08248dd2acf8bb1a17ca187672d51b16477
Luca Hartman <lucahartman@tsf-428-wpa-4-240.epfl.ch>
2022-04-29 16:01:49 +0200

    track forward modelling working on test

--- fonctions.py
+++ fonctions.py
@@ -3,6 +3,13 @@ import numpy as np
 from astropy.io import fits
 import scipy.optimize as opt
 from photutils.segmentation import make_source_mask
+from scipy import stats
+from scipy.signal import convolve2d
+import numpy as np
+from astropy.io import fits
+from astropy.visualization import astropy_mpl_style
+import matplotlib.pyplot as plt
+plt.style.use(astropy_mpl_style)
 
 def calibrate(img, bias, dark, flat):
     """ Image (img) calibration using a bias frame, a dark frame and a flat frame, the last three being average of multiple images
@@ -114,3 +121,81 @@ def fitForAllStars(image, list_of_coordinates):
     #interpolated_data = gaussian2D(xy, params[0], params[1], params[2], params[3], params[4], params[5])
     return parameters
 
+# fonctions to create and fit model of track
+def trackModel(shape, i0, j0, iend, jend, width, amplitude):
+    """ Creates a model of track in a frame of a given shape starting in position (i0, j0) and going in a straight line to (iend, jend) with a given width and amplitude
+        shape: (lines, colunms), usually of np.shape()
+        i0, j0, iend, jend: integer coordinates, inside of shape (including boundaries)
+        width: scalar of width of track, measured perpendicular to the track
+        amplitude: scalar, amplitude of track
+    
+    Return: outcut: matrix of size shape containing the track and zero outside
+    """
+    outcut = np.zeros(shape)  # create empty matrix
+    L = np.sqrt((iend - i0)**2 + (jend - j0)**2)  # length of the track
+    # angle of the track w.r.t. horizontal line
+    alpha = np.arctan((iend - i0)/(jend - j0))
+    # projection of the width along the vertical axis
+    width_i = int(width/2 * np.cos(alpha))
+    # projection of the width along the horizontal axis
+    width_j = int(width/2 * np.sin(alpha))
+    for t in range(int(np.floor(L))):  # along the track
+        i = int(i0 + (iend - i0)/L * t)
+        j = int(j0 + (jend - j0)/L * t)
+        # for now: amplitude inside all the track +- width, maybe gaussian distribution ?
+        outcut[i, j] = amplitude
+        if width_i > 0 and (i - width_i) >= 0 and (i + width_i) <= shape[0]:
+            outcut[i-width_i:i+width_i, j] = amplitude
+        if width_j > 0 and (j - width_j) >= 0 and (j + width_j) <= shape[1]:
+            outcut[i, j-width_j:j+width_j]
+    return outcut
+
+
+def reducedGoodnessModel(outcut, model, error):
+    """ Measures goodness of track model compared to the outcut of the image containing the track, using chi2-test
+        outcut: part of image containing the track
+        model: track model
+    Return: chi2: value of the chi2-test
+            p: p-value of the chi2-test, assuming track model 6 parameters
+    """
+    #return chisquare(np.ravel(outcut), np.ravel(model), 6)
+    if np.shape(outcut) != np.shape(model):
+        raise ValueError('Not same shapes')
+    mask = make_source_mask(outcut, nsigma=4, npixels=1000, dilate_size=2)
+    track = np.ma.array(outcut, mask=np.logical_not(mask), fill_value=0)
+    measured = np.ravel(track.filled())
+    simulated = np.ravel(model)
+    # simply chi^2 test, from wikipedia
+    chi2 = np.sum((simulated - measured)**2/np.ravel(error))
+    p = stats.chi2.cdf(chi2, len(simulated)-6)  # assuming 6 parameters
+    return chi2/len(simulated)
+
+def logLikelihood(theta, y, yerr, PSF):
+    """ Computes the log likelihood of the model specified by the parameters theta for the variables x (raveled coordinates of the outcut) and the measurements y (raveled outcut)
+        theta: list of parameters (i0, j0, iend, jend, width, amplitude)
+        x: indices of the raveled outcut model
+        y: raveled outcut
+    Return: -0.5*chi^2
+    """
+    shape = np.shape(y)
+    i0, j0, iend, jend, width, amplitude = theta
+    y_model = trackModel(shape, i0, j0, iend, jend, width, amplitude)
+    return -0.5*reducedGoodnessModel(y, convolve2d(y_model, PSF, 'same'), yerr)
+
+
+def logPrior(theta):
+    """ Computes the prior probability of a given set of parameters theta (here assuming uniform distribution)
+    return: scalar
+    """
+    i0, j0, iend, jend, width, amplitude = theta
+    if 0 <= i0 <= 500 and 0 <= iend <= 500 and 0 <= j0 <= 500 and 0 <= jend <= 500 and 0 < width < 10 and 0 < amplitude < 65535:
+        return 0 # log(probability) up to a constant
+    else:
+        return -np.inf
+
+
+def logProbability(theta, y, yerr, PSF):
+    lp = logPrior(theta)
+    if np.isfinite(lp):
+        return lp + logLikelihood(theta, y, yerr, PSF)
+    return -np.inf
