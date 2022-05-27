dabd369e297f883108dbb194436f413bfb0c87bf
Luca Hartman <lucahartman@tsf-428-wpa-1-175.epfl.ch>
2022-05-27 09:55:57 +0200

    correction large stars

--- PSF-map-code.py
+++ PSF-map-code.py
@@ -3,12 +3,7 @@ from astropy.io import fits
 from astropy.visualization import astropy_mpl_style
 import matplotlib.pyplot as plt
 plt.style.use(astropy_mpl_style)
-
-# Prepare for calibration
 from fonctions import *
-bias = fits.getdata('bias.fit')
-dark = fits.getdata('dark.fit')
-flat = fits.getdata('flat.fit')
 
 bias = averageFolder('bias')
 dark = averageFolder('dark')
@@ -19,34 +14,30 @@ sigma_x_map = np.zeros(np.shape(flat))
 sigma_y_map = np.zeros(np.shape(flat))
 theta_map = np.zeros(np.shape(flat))
 
-# , 'image2.fit', 'image3.fit', 'image4.fit', 'image5.fit', 'image6.fit', 'image7.fit', 'image8.fit']
-image_names = ['image4.fit', 'image6.fit', 'image7.fit', 'image8.fit']
+# getting the names of all images
+image_names = []
+for file in os.listdir('../psf-map-images/'):
+    if file.endswith(".fit"):
+        image_names.append(os.path.join('../psf-map-images/', file))
 
-for image_name in image_names:
-    print('\tImage name : ', image_name)
+# for each image, find all the stars, fit each star and add the parameters to the map
+for image_name in tqdm(image_names, desc='Image names', leave=True):
     img = fits.getdata(image_name)
     img = calibrate(img, bias, dark, flat)
     list_of_coordinates = coordinatesOfStars(img)
-    parameters = []
-    n_old, n = 0, 1000
-    while n < len(list_of_coordinates):
-        print('\tn_old and n : ', n_old, n)
-        parameters += fitForAllStars(img, list_of_coordinates[n_old:n-1])
-        n_old = n
-        n += 1000
-    if n > len(list_of_coordinates):
-        parameters += fitForAllStars(img, list_of_coordinates[n_old:])                        
-    
+    parameters = fitForAllStars(img, list_of_coordinates)                        
     for i in range(len(parameters)):
         coords = list_of_coordinates[i]
         i_begin = np.max([0, coords[0]-20])
         i_end = np.min([np.shape(img)[0], coords[0]+20])
         j_begin = np.max([0, coords[1]-20])
         j_end = np.min([np.shape(img)[1], coords[1]+20])
-        sigma_x_map[i_begin:i_end, j_begin:j_end] += parameters[i][2]/len(image_names)
-        sigma_y_map[i_begin:i_end, j_begin:j_end] += parameters[i][3]/len(image_names)
-        theta_map[i_begin:i_end, j_begin:j_end] += parameters[i][4]/len(image_names)
-        
+        if parameters[2] < 30 and parameters[3] < 30:
+            sigma_x_map[i_begin:i_end, j_begin:j_end] = parameters[i][2]
+            sigma_y_map[i_begin:i_end, j_begin:j_end] = parameters[i][3]
+            theta_map[i_begin:i_end, j_begin:j_end] = parameters[i][4]
+   
+# saving the maps     
 textfile = open("sigma_x_map.dat", "w")
 sigma_x_map.tofile(textfile)
 textfile.close()
